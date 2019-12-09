51b699b7a68aaa29d101d1552b99784d10cc2fb9
Ervin Pepic <ervin.hack@gmail.com>
2019-12-09 12:10:23 +0100

    one more task

--- datetime_tasks.js
+++ datetime_tasks.js
@@ -220,3 +220,26 @@ function getSecondsToTomorrow() {
     return Math.round(razlika / 1000);
 }
 console.log(getSecondsToTomorrow());
+
+
+
+function formatDate(date) {
+    let diff = new Date() - date; // the difference in milliseconds
+  
+    if (diff < 1000) { // less than 1 second
+      return 'right now';
+    }
+  
+    let sec = Math.floor(diff / 1000); // convert diff to seconds
+  
+    if (sec < 60) {
+      return sec + ' sec. ago';
+    }
+  
+    let min = Math.floor(diff / 60000); // convert diff to minutes
+    if (min < 60) {
+      return min + ' min. ago';
+    }
+}
+
+console.log(formatDate(new Date(new Date - 86400 * 1000)));
