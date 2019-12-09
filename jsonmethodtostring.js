e3a90d4331bf360146ab9184001218e215b48d77
Ervin Pepic <ervin.hack@gmail.com>
2019-12-09 22:16:58 +0100

    Added more examples for stringify js objects

--- jsonmethodtostring.js
+++ jsonmethodtostring.js
@@ -21,3 +21,82 @@ let jsonConvert = JSON.stringify(student);
 
 console.log(typeof jsonConvert);
 console.log(jsonConvert);
+
+let korisnik = {
+    pozdrav() {
+        return console.log('Hello');
+    },
+    [Symbol('id')]: 123,
+    nekosvojstvo: undefined
+}
+
+console.log(JSON.stringify(korisnik));
+
+let sastanak = {
+    naslov: 'Conference',
+    soba: {
+        broj: 23,
+        pretplatnici: ['erivn', 'emel']
+    }
+};
+
+console.log(JSON.stringify(sastanak));
+
+
+// let room = {
+//     number: 23
+//   };
+  
+//   let meetup = {
+//     title: "Conference",
+//     participants: ["john", "ann"]
+//   };
+  
+//   meetup.place = room;       // meetup references room
+//   room.occupiedBy = meetup;
+
+//   console.log(meetup);
+
+let room = {
+    number: 23
+};
+
+let meetup = {
+    title: 'Conference',
+    participants: [{name: 'Ervin'}, {name: 'Emel'}],
+    place: room,
+};
+
+room.occupiedBy = meetup; //room references meetup
+
+console.log(JSON.stringify(meetup, function zamejnikzaniz(kljuc, vrijednost){
+    console.log(`${kljuc}: ${vrijednost}`);
+    return (kljuc == 'occupiedBy') ? undefined : vrijednost;
+}));
+
+let formatiranje = {
+    ime: 'Ervin',
+    prezime: 'Pepic',
+    ostaliPodaci: {
+        isAdmin: true,
+        isEditor: false,
+    }
+};
+
+console.log(JSON.stringify(formatiranje, null, 4));
+
+
+let apartmani = {
+    sobe: 23,
+    toJSON() {
+        return this.sobe;
+    }
+};
+
+let seminar = {
+    title: 'Conference',
+    apartmani
+};
+
+console.log(JSON.stringify(seminar));
+
