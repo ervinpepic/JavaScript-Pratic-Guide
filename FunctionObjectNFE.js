3c8e3d54fe7f349a51f579fd369be5f1c4355f7e
Ervin Pepic <ervin.hack@gmail.com>
2019-12-30 22:41:01 +0100

    Finshed chapter with all tasks

--- FunctionObjectNFE.js
+++ FunctionObjectNFE.js
@@ -87,3 +87,78 @@ let counter = makeCounter();
 
 console.log(counter());
 console.log(counter());
+
+
+// Named Function Expression, or NFE, is a term for Function Expressions that have a name.
+
+// For instance, let’s take an ordinary Function Expression:
+
+let naziv = function reciZdravo(ko){
+    console.log(`zdravo ${ko}`);
+}
+
+//or
+
+let naziv1 = function func(ko) {
+    console.log(`zdravo ${ko}`);
+}
+
+naziv('Ervin');
+naziv1('Erevin');
+
+
+let pozdrav2 = function func(who) {
+    if (who) {
+        console.log(`Hello ${who}`);
+    } else {
+        func("Guest");
+    }
+}
+pozdrav2();
+
+
+
+function makeCounterTask(){
+    let count = 0;
+
+    function counter() {
+        return count++;
+    }
+    counter.set = value => count = value;
+    counter.decrease = () => count--;
+
+    return counter;
+
+}
+
+
+// Write function sum that would work like this:
+
+// sum(1)(2) == 3; // 1 + 2
+// sum(1)(2)(3) == 6; // 1 + 2 + 3
+// sum(5)(-1)(2) == 6
+// sum(6)(-1)(-2)(-3) == 0
+// sum(0)(1)(2)(3)(4)(5) == 15
+// P.S. Hint: you may need to setup custom object to primitive conversion for your function.
+
+
+function sum(a) {
+    let currentSum = a;
+
+    function f(b) {
+        currentSum += b;
+        return f;
+    }
+
+    f.toString = function() {
+        return currentSum;
+    }
+
+    return f;
+}
+
+console.log(sum(1)(2)); // 1 + 2
+console.log(sum(1)(2)(3)); // 1 + 2 + 3
+console.log(sum(5)(-1)(2));
+console.log(sum(6)(-1)(-2)(-3));
+console.log(sum(0)(1)(2)(3)(4)(5));
