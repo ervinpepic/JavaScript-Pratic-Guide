e5aa5bddbd90b919e6e794ed9b78868e898f9177
Ervin Pepic <ervin.hack@gmail.com>
2020-01-06 15:55:09 +0100

    Finished Decorator&Forwarding chapter with tasks

--- DecoratorsAndForwardingCallApply.js
+++ DecoratorsAndForwardingCallApply.js
@@ -11,7 +11,7 @@ function slow(x) {
 function cachingDecorator(yourFuncName) {
     let cache = new Map();
 
-    return function(localParam) {
+    return function (localParam) {
         if (cache.has(localParam)) {
             return cache.get(localParam);
         }
@@ -47,7 +47,7 @@ let worker = {
 function cachingDecorator1(yourFuncName) {
     let cache = new Map();
 
-    return function(localParam) {
+    return function (localParam) {
         if (cache.has(localParam)) {
             return cache.get(localParam);
         }
@@ -86,3 +86,167 @@ function say(phrase) {
 }
 let user1 = ('Ervin Pepic');
 say.call(user, 'Hello');
+
+let radnik = {
+    slow2(min, max) {
+        return min + max;
+    }
+}
+
+radnik.slow2 = cachingDecorator1(radnik.slow2);
+
+// Going multi-argument with “func.apply”
+
+
+let radnici = {
+    slowFunc(min, max) {
+        console.log(`Called with ${min}, ${max}`);
+        return min + max;
+    }
+};
+
+function cchhDecorator(functionToProvide, hashableFunction) {
+    let kes = new Map();
+    return function () {
+        let kljuc = hashableFunction(arguments)
+        if (kes.has(kljuc)) {
+            return kes.get(kljuc);
+        }
+        let rezultat = functionToProvide.call(this, ...arguments); //(**)
+
+        kes.set(kljuc, rezultat);
+        return rezultat
+    };
+}
+
+function hashableFunction(args) {
+    return args[0] + ', ' + args[1];
+}
+
+radnici.slowFunc = cchhDecorator(radnici.slowFunc, hashableFunction);
+
+console.log(radnici.slowFunc(3, 5));
+console.log('Ponovo: ' + radnici.slowFunc(3, 5));
+
+
+// it runs the func setting this=context and using an array-like object args as the list of arguments.
+
+// The only syntax difference between call and apply is that call expects a list of arguments,
+//  while apply takes an array-like object with them.
+
+// So these two calls are almost equivalent:
+
+// func.call(context, ...args); // pass an array as list with spread syntax
+// func.apply(context, args);   // is same as using apply
+
+// There’s only a minor difference:
+
+// The spread syntax ... allows to pass iterable args as the list to call.
+// The apply accepts only array-like args.
+
+//we can improve our cahce function to accpet more than just two arguments like this:
+
+function hashFunc() {
+    console.log([].join.call(arguments));
+}
+
+hashFunc(1, 2);
+
+// Create a decorator spy(func) that should return a wrapper that saves all calls to function in its calls property.
+
+// Every call is saved as an array of arguments.
+
+// For instance:
+
+
+// function work(a, b) {
+//   alert( a + b ); // work is an arbitrary function or method
+// }
+
+// work = spy(work);
+
+// work(1, 2); // 3
+// work(4, 5); // 9
+
+// for (let args of work.calls) {
+//   alert( 'call:' + args.join() ); // "call:1,2", "call:4,5"
+// }
+
+function spy(callFunc) {
+
+    function wrapper(...args) {
+        wrapper.calls.push(args);
+        return callFunc.apply(this, args);
+    }
+
+
+    wrapper.calls = [];
+    return wrapper;
+
+}
+
+
+//second task
+
+
+function delayMs(functionToDelay, miliseconds) {
+
+    return function () {
+        setTimeout(() => functionToDelay.apply(this, arguments), miliseconds);
+    };
+
+}
+
+let f1000 = delayMs(console.log, 1000);
+f1000('Ervin');
+
+//another solution but so cumbersome
+
+function delayMiliseconds(functionToDelay, miliseconds) {
+
+    return function (...args) {
+        let savedThis = this;
+        setTimeout(function () {
+            functionToDelay.apply(savedThis, args);
+        }, miliseconds);
+    };
+}
+
+//third task
+
+function debounceMs(funtctionToProvide, seconds) {
+    let isCooldown = false;
+
+    return function () {
+
+        if (isCooldown) return;
+        functionToProvide.apply(this, arguments);
+        isCooldown = true;
+
+        setTimeout(() => isCooldown = false, seconds);
+    };
+}
+
+//forth
+
+function thortle(func, ms) {
+    let isThortled = false, savedArgs, savedThis;
+
+    function wrapper() {
+        if (isThortled) {
+            savedThis = this;
+            savedArgs = arguments;
+            return;
+        }
+        func.apply(this, arguments);
+        isThortled = true;
+        setTimeout(function () {
+            isThortled = false;
+            if (savedArgs) {
+                wrapper.apply(savedThis, savedArgs);
+                savedArgs = savedThis = null;
+            }
+        }, ms);
+    }
+    return wrapper
+}
